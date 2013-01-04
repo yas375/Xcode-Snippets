e4343fabff3273be44b90b49ff75896f334ba37f
Nicolas Bouilleaud <nicolas.bouilleaud@gmail.com>
2013-01-04 11:46:50 +0100

    Convert .m files to .codesnippet files

--- strongself.m
+++
@@ -1,8 +0,0 @@
-// __strong self
-// Declare strong referece to weak reference
-// 
-// Platform: All
-// Language: Objective-C
-// Completion Scope: Function or Method
-
-__strong __typeof(&*weakSelf)strongSelf = <#weakSelf#>;
--- weakself.m
+++
@@ -1,8 +0,0 @@
-// __weak self
-// Declare weak referece to self
-// 
-// Platform: All
-// Language: Objective-C
-// Completion Scope: Function or Method
-
-__weak __typeof(&*self)weakSelf = self;
